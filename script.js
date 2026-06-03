f608b2a6b043f4a6f3aed469d534fbb33cebf395
goodytecheditz <goodytecheditz@gmail.com>
2026-06-03 16:45:10 +0100

    Update script.js
    Signed-off-by: goodytecheditz <goodytecheditz@gmail.com>

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-function showSupport() {
-    var msg = document.getElementById("support-msg");
-    if (msg.style.display === "none") {
-        msg.style.display = "block";
-    } else {
-        msg.style.display = "none";
-    }
-}
+function sendToWhatsApp() {
+    var name = document.getElementById("clientName").value;
+    var event = document.getElementById("eventType").value;
+    var msg = document.getElementById("message").value;
 
-document.getElementById("f").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Message sent to the CEO!");
-    this.reset();
-});
+    if (name === "" || event === "" || msg === "") {
+        alert("Please fill in all fields!");
+        return;
+    }
+
+    var fullMessage = "Hello CEO, I am " + name + ".%0A%0A" +
+                      "Event Type: " + event + "%0A" +
+                      "Message: " + msg;
+
+    var whatsappURL = "https://wa.me/2347072539979?text=" + fullMessage;
+    
+    // This changes the current page to WhatsApp, which mobile browsers allow
+    window.location.href = whatsappURL;
+}
